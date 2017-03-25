873f578dc31f51eb6cd2f70f0a44063c68341f22
Andvri <javier.martinez@unet.edu.ve>
2017-03-25 15:05:43 -0400

    Agregado el control y rotacion por la pantalla tactil
    Mejorar la estructura de codigo y Calculo en el vector de rotacion

--- Assets/Scripts/Controller/Entities/TableController.cs
+++ Assets/Scripts/Controller/Entities/TableController.cs
@@ -18,7 +18,15 @@ public class TableController : MonoBehaviour {
         float moveH = CrossPlatformInputManager.GetAxis("Horizontal");
         float newX = (moveH * Time.deltaTime * 50)+ rig.transform.position.x;
         rig.transform.position = new Vector3(Mathf.Clamp(newX, -10, 10), transform.position.y, transform.position.z);
-        particles.transform.Rotate(new Vector3(0, Mathf.Clamp(moveH*-40, -40, 40), 0)) ;
+
+      //  particles.transform.Rotate(new Vector3(0,-1, 0), moveH*-40*Time.deltaTime);
+
+       particles.transform.rotation.SetAxisAngle(new Vector3(0,-1,0), moveH * -0 * Time.deltaTime);
+   //     currentRotation.y = Mathf.Clamp(currentRotation.y, -40, 40);
+
+   //     particles.transform.rotation = Quaternion.Euler(currentRotation);
+       
+       
     }
 
     // Si la clase MonoBehaviour está habilitada, se llama a esta función en cada fotograma de velocidad de fotograma fija
---
+++ Assets/Scripts/touch.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class touch : MonoBehaviour {
+    public GameObject tabla;
+    public Transform cilindro;
+    public Vector2 origin;
+    public Vector2 end;
+    public Vector3 rotationg;
+    public float angulo;
+    public Color color;
+    public Rigidbody CRPelota;
+
+    // Use this for initialization
+    void Start () {
+        color = Color.white;
+        origin = end = new Vector2( Screen.height / 2, Screen.width/2 );
+        rotationg = Vector3.zero;
+        angulo = 0;
+    }
+
+ 
+    // Update is called once per frame
+    void Update () {
+      //  Debug.Log(" Height: " + Screen.height + " Width: " + Screen.width);
+        foreach (var touchactual in Input.touches)
+        {
+            if(touchactual.tapCount == 2)
+            {
+                Debug.Log("fUEGO");
+                Debug.Log("Rotacion de la pelota"+cilindro.rotation);
+                
+                CRPelota.AddForce(new Vector3(100*cilindro.rotation.y,0,60));
+            }
+           
+           // Debug.Log(" PHeight: " + touchactual.position.x + " PWidth: " + touchactual.position.y);
+            if (touchactual.phase == TouchPhase.Began)
+            {
+              //  rotationg = new Vector3(0, touchactual.position.x, touchactual.position.y);
+              //  angulo = Vector3.Angle(rotationg, Vector3.forward);
+              //  cilindro.Rotate(Vector3.up,angulo-30);    
+            }
+
+            if (touchactual.phase == TouchPhase.Stationary)
+            {
+                rotationg = new Vector3(0, touchactual.position.x, touchactual.position.y);
+                angulo = Vector3.Angle(rotationg, Vector3.forward);
+                cilindro.Rotate(Vector3.up, (angulo - 30)/10);
+                //Debug.Log(cilindro.rotation.y);
+                if( cilindro.rotation.y > 0.6)
+                {
+                    cilindro.Rotate(Vector3.up, 100*(0.6f - cilindro.rotation.y));
+                }
+                if(cilindro.rotation.y < -0.6f)
+                {
+                    cilindro.Rotate(Vector3.up, -100 * (0.6f +cilindro.rotation.y));
+                }
+
+            }
+            
+            if (touchactual.phase == TouchPhase.Moved)
+            {
+                
+                float moveH = touchactual.deltaPosition.x;
+                Transform moveOb = tabla.GetComponent<Transform>();
+                float newX = (moveH * Time.deltaTime * 3) + moveOb.position.x;
+                moveOb.transform.position = new Vector3(Mathf.Clamp(newX, -5, 5), moveOb.transform.position.y, moveOb.transform.position.z);
+
+                
+            }  
+             
+        }
+
+	
+	}
+
+}
