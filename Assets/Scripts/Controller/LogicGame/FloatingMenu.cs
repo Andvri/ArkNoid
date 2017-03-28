00c3962a66a0ac5f10ad5f35d719eaeace9e3c38
Andvri <javier.martinez@unet.edu.ve>
2017-03-27 20:20:13 -0400

    Floating menus

---
+++ Assets/Scripts/Controller/LogicGame/FloatingMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingMenu : MonoBehaviour {
+    public GameObject mainMenu;
+    public GameObject playMenu;
+    public GameObject credits;
+    
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void starPlayMenu()
+    {
+        playMenu.SetActive(true);
+    }
+    public void exitPlayMenu()
+    {
+        playMenu.SetActive(false);
+    }
+    public void starArcadeMode()
+    {
+        //cambiar a scene1
+    }
+    public void starCreditsMenu()
+    {
+        credits.SetActive(true);
+    }
+    public void exitCreditsMenu()
+    {
+        credits.SetActive(false);
+    } 
+
+
+}
--- Assets/Scripts/touch.cs
+++ Assets/Scripts/touch.cs
@@ -4,17 +4,16 @@ using System.Collections;
 public class touch : MonoBehaviour {
     public GameObject tabla;
     public Transform cilindro;
-    public Vector2 origin;
-    public Vector2 end;
     public Vector3 rotationg;
     public float angulo;
-    public Color color;
+    public float LimitA;
+    public float Velocit;
     public Rigidbody CRPelota;
 
     // Use this for initialization
     void Start () {
-        color = Color.white;
-        origin = end = new Vector2( Screen.height / 2, Screen.width/2 );
+        Velocit = 300f;
+        LimitA = 0.6f;
         rotationg = Vector3.zero;
         angulo = 0;
     }
@@ -22,52 +21,48 @@ public class touch : MonoBehaviour {
  
     // Update is called once per frame
     void Update () {
-      //  Debug.Log(" Height: " + Screen.height + " Width: " + Screen.width);
+     
         foreach (var touchactual in Input.touches)
         {
             if(touchactual.tapCount == 2)
             {
-                Debug.Log("fUEGO");
-                Debug.Log("Rotacion de la pelota"+cilindro.rotation);
-                
-                CRPelota.AddForce(new Vector3(100*cilindro.rotation.y,0,60));
+                float ang = (((cilindro.rotation.y * 100) *Mathf.PI)/180f);
+                Vector3 force = new Vector3(Velocit*Mathf.Sin(ang), 0,Velocit*Mathf.Cos(ang));
+                CRPelota.AddForce(force);
+                break;
             }
            
-           // Debug.Log(" PHeight: " + touchactual.position.x + " PWidth: " + touchactual.position.y);
             if (touchactual.phase == TouchPhase.Began)
             {
-              //  rotationg = new Vector3(0, touchactual.position.x, touchactual.position.y);
-              //  angulo = Vector3.Angle(rotationg, Vector3.forward);
-              //  cilindro.Rotate(Vector3.up,angulo-30);    
+                
             }
-
-            if (touchactual.phase == TouchPhase.Stationary)
-            {
-                rotationg = new Vector3(0, touchactual.position.x, touchactual.position.y);
-                angulo = Vector3.Angle(rotationg, Vector3.forward);
-                cilindro.Rotate(Vector3.up, (angulo - 30)/10);
-                //Debug.Log(cilindro.rotation.y);
-                if( cilindro.rotation.y > 0.6)
-                {
-                    cilindro.Rotate(Vector3.up, 100*(0.6f - cilindro.rotation.y));
-                }
-                if(cilindro.rotation.y < -0.6f)
-                {
-                    cilindro.Rotate(Vector3.up, -100 * (0.6f +cilindro.rotation.y));
-                }
-
-            }
-            
             if (touchactual.phase == TouchPhase.Moved)
             {
-                
+
                 float moveH = touchactual.deltaPosition.x;
                 Transform moveOb = tabla.GetComponent<Transform>();
                 float newX = (moveH * Time.deltaTime * 3) + moveOb.position.x;
                 moveOb.transform.position = new Vector3(Mathf.Clamp(newX, -5, 5), moveOb.transform.position.y, moveOb.transform.position.z);
 
-                
-            }  
+                break;
+            }
+            if (touchactual.phase == TouchPhase.Stationary)
+            {
+                rotationg = new Vector3(0, touchactual.position.x, touchactual.position.y);
+                angulo = Vector3.Angle(rotationg, Vector3.forward);
+                cilindro.Rotate(Vector3.up, (angulo - 30)/10);
+                if( cilindro.rotation.y > LimitA)
+                {
+                    cilindro.Rotate(Vector3.up, 100*(LimitA - cilindro.rotation.y));
+                }
+                if(cilindro.rotation.y < -LimitA)
+                {
+                    cilindro.Rotate(Vector3.up, -100 * (LimitA +cilindro.rotation.y));
+                }
+                break;
+            }
+            
+            
              
         }
 
