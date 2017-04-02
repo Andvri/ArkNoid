5011ee8df59b1b9aca7afc777ee812f8ab783e53
Andvri <javier.martinez@unet.edu.ve>
2017-04-02 11:13:05 -0400

    Add Scrip move const ent

---
+++ Assets/Scripts/Controller/Entities/MoveEnt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnt : MonoBehaviour {
+    public Vector3 Direccion;
+    public int ida;
+    public int Desplazamiento;
+	// Use this for initialization
+	void Start () {
+        Desplazamiento *= 10;
+        ida = Desplazamiento;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos_ori = transform.position;
+        if (ida >0){
+            
+            transform.position = pos_ori +(Direccion*Time.deltaTime);
+            
+        }
+        if (ida <= 0){
+            transform.position = pos_ori - (Direccion * Time.deltaTime);
+        }
+        ida--;
+        if (ida <= -Desplazamiento)
+            ida = Desplazamiento;
+
+    }
+}
